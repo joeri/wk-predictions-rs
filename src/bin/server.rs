f8c0b137be7c7bde7206ebfb21f4e938d4353a52
Joeri Samson <joerisamson@gmail.com>
2018-04-19 09:15:36 +0200

    Start thinking about logging in/registration
    Added serde dependency to be able to parse form data

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,10 @@
 extern crate actix;
 extern crate actix_web;
-extern crate futures;
 extern crate diesel;
 extern crate dotenv;
+extern crate futures;
+#[macro_use]
+extern crate serde_derive;
 
 extern crate wk_predictions;
 use wk_predictions::schema;
@@ -13,7 +15,8 @@ use diesel::pg::PgConnection;
 use dotenv::dotenv;
 use std::env;
 
-use actix_web::{http, server, App, State, Error, HttpResponse, Path, AsyncResponder, FutureResponse};
+use actix_web::{http, server, App, AsyncResponder, Error, Form, FutureResponse, HttpResponse,
+                Path, State};
 
 use actix::prelude::*;
 
@@ -52,16 +55,51 @@ struct AppState {
     db: Addr<Syn, DbExecutor>,
 }
 
+#[derive(Deserialize)]
+struct LoginForm {
+    username: String,
+    password: String,
+}
+
+#[derive(Deserialize)]
+struct RegistrationForm {
+    username: String,
+    name: String,
+    password: String,
+}
+
 fn index(info: Path<(String, u32)>, state: State<AppState>) -> FutureResponse<HttpResponse> {
     state
         .db
         .send(CountUsers {})
         .from_err()
         .and_then(move |res| match res {
-            Ok(count) => Ok(HttpResponse::Ok().body(format!("There are {} user(s), data = {}, {}", count, info.0, info.1))),
+            Ok(count) => Ok(HttpResponse::Ok().body(format!(
+                "There are {} user(s), data = {}, {}",
+                count, info.0, info.1
+            ))),
             Err(_) => Ok(HttpResponse::InternalServerError().into()),
         })
         .responder()
+}
+
+fn login(state: State<AppState>) -> HttpResponse {
+    HttpResponse::Ok().body("login")
+}
+
+fn perform_login(form: Form<LoginForm>, state: State<AppState>) -> FutureResponse<HttpResponse> {
+    futures::future::ok(HttpResponse::Ok().body("POST login")).responder()
+}
+
+fn register(state: State<AppState>) -> FutureResponse<HttpResponse> {
+    futures::future::ok(HttpResponse::Ok().body("GET register")).responder()
+}
+
+fn perform_registration(
+    form: Form<RegistrationForm>,
+    state: State<AppState>,
+) -> FutureResponse<HttpResponse> {
+    futures::future::ok(HttpResponse::Ok().body("POST register")).responder()
 }
 
 fn main() {
@@ -81,7 +119,16 @@ fn main() {
 
     server::new(move || {
         App::with_state(AppState { db: addr.clone() })
-            .resource("/{name}/{id}/index.html", |r| r.method(http::Method::GET).with2(index))
+            .resource("/login", |r| {
+                r.name("login");
+                r.get().with(login);
+                r.post().with2(perform_login).0.limit(4096);
+            })
+            .resource("/register", |r| {
+                r.get().with(register);
+                r.post().with2(perform_registration).0.limit(4096);
+            })
+            .resource("/{name}/{id}/index.html", |r| r.get().with2(index))
     }).bind(&url)
         .unwrap()
         .start();
