aaec0223fa1cf0a28f32f0dba62758f14230ac8f
Joeri Samson <joerisamson@gmail.com>
2018-05-22 09:51:14 +0200

    Don't compile swp files

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -2,7 +2,7 @@ pub use tera::{Context, Tera};
 
 lazy_static! {
     pub static ref TEMPLATE_SERVICE: Tera = {
-        let tera = compile_templates!("templates/**/*");
+        let tera = compile_templates!("templates/**/*.html");
         tera
     };
 }
