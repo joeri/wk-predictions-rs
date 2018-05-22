4be5000f80db7eba8d29377d34cc88d3fa7cedfc
Joeri Samson <joerisamson@gmail.com>
2018-05-22 09:45:19 +0200

    Cargo fmt on import_data

--- src/bin/import_data.rs
+++ src/bin/import_data.rs
@@ -1,14 +1,14 @@
+extern crate csv;
 extern crate diesel;
 extern crate dotenv;
-extern crate csv;
 #[macro_use]
 extern crate serde_derive;
 extern crate chrono;
 
 extern crate wk_predictions;
 
-use diesel::prelude::*;
 use diesel::insert_into;
+use diesel::prelude::*;
 use dotenv::dotenv;
 
 use std::env;
@@ -18,7 +18,6 @@ use std::path::Path;
 use std::collections::HashMap;
 
 use wk_predictions::{schema, models::*};
-
 
 #[derive(Deserialize, Debug)]
 struct CountryRow {
@@ -42,7 +41,12 @@ fn import_countries(conn: &PgConnection) -> Result<Vec<Country>, Box<Error>> {
                 countries::flag.eq(record.flag),
                 countries::seeding_pot.eq(record.seeding_pot),
             ))
-            .returning((countries::country_id, countries::name, countries::flag, countries::seeding_pot))
+            .returning((
+                countries::country_id,
+                countries::name,
+                countries::flag,
+                countries::seeding_pot,
+            ))
             .get_result(conn)?;
         println!("{:?}", inserted);
         result.push(inserted);
@@ -67,9 +71,7 @@ fn import_groups(conn: &PgConnection) -> Result<Vec<Group>, Box<Error>> {
         let record = row?;
         println!("{:?}", record);
         let inserted = insert_into(groups::table)
-            .values((
-                groups::name.eq(record.name),
-            ))
+            .values((groups::name.eq(record.name),))
             .returning((groups::group_id, groups::name))
             .get_result(conn)?;
         println!("{:?}", inserted);
@@ -87,8 +89,8 @@ struct GroupMembershipRow {
 }
 
 fn import_group_memberships(conn: &PgConnection) -> Result<Vec<GroupMembership>, Box<Error>> {
-    use schema::{group_memberships, groups, countries};
     use diesel::dsl::sql;
+    use schema::{countries, group_memberships, groups};
 
     let mut result = Vec::new();
 
@@ -96,12 +98,29 @@ fn import_group_memberships(conn: &PgConnection) -> Result<Vec<GroupMembership>,
     for row in rdr.deserialize::<GroupMembershipRow>() {
         let record = row?;
         println!("{:?}", record);
-        let new_groups = groups::table.filter(groups::name.eq(record.group)).inner_join(countries::table.on(countries::name.eq(record.country)));
+        let new_groups = groups::table
+            .filter(groups::name.eq(record.group))
+            .inner_join(countries::table.on(countries::name.eq(record.country)));
 
         let inserted = insert_into(group_memberships::table)
-            .values(new_groups.select((groups::group_id, countries::country_id, sql(&record.drawn_place), sql(&record.drawn_place))))
-            .into_columns((group_memberships::group_id, group_memberships::country_id, group_memberships::drawn_place, group_memberships::current_position))
-            .returning((group_memberships::country_id, group_memberships::group_id, group_memberships::drawn_place, group_memberships::current_position))
+            .values(new_groups.select((
+                groups::group_id,
+                countries::country_id,
+                sql(&record.drawn_place),
+                sql(&record.drawn_place),
+            )))
+            .into_columns((
+                group_memberships::group_id,
+                group_memberships::country_id,
+                group_memberships::drawn_place,
+                group_memberships::current_position,
+            ))
+            .returning((
+                group_memberships::country_id,
+                group_memberships::group_id,
+                group_memberships::drawn_place,
+                group_memberships::current_position,
+            ))
             .get_result(conn)?;
         println!("{:?}", inserted);
         result.push(inserted);
@@ -190,7 +209,12 @@ fn import_stages(conn: &PgConnection) -> Result<Vec<Stage>, Box<Error>> {
                 stages::stage_type.eq(stage_type),
                 stages::description.eq(record.description),
             ))
-            .returning((stages::stage_id, stages::parent_stage_id, stages::stage_type, stages::description))
+            .returning((
+                stages::stage_id,
+                stages::parent_stage_id,
+                stages::stage_type,
+                stages::description,
+            ))
             .get_result(conn)?;
         println!("{:?}", inserted);
         result.push(inserted);
@@ -209,13 +233,18 @@ struct MatchRow {
     city: String,
 }
 
-fn import_matches(locations: Vec<Location>, countries: Vec<Country>, conn: &PgConnection) -> Result<(Vec<Match>, Vec<(MatchParticipant, MatchParticipant)>), Box<Error>> {
-    use schema::{matches, match_participants, group_memberships};
+fn import_matches(
+    locations: Vec<Location>,
+    countries: Vec<Country>,
+    conn: &PgConnection,
+) -> Result<(Vec<Match>, Vec<(MatchParticipant, MatchParticipant)>), Box<Error>> {
     use chrono::DateTime;
+    use schema::{group_memberships, match_participants, matches};
 
     let mut locations_by_city_and_stadium = HashMap::with_capacity(locations.len());
     for location in locations {
-        locations_by_city_and_stadium.insert((location.city.clone(), location.stadium.clone()), location);
+        locations_by_city_and_stadium
+            .insert((location.city.clone(), location.stadium.clone()), location);
     }
 
     let mut teams_by_country_name = HashMap::with_capacity(countries.len());
@@ -233,15 +262,21 @@ fn import_matches(locations: Vec<Location>, countries: Vec<Country>, conn: &PgCo
 
         let (group_id, drawn_place) = group_memberships::table
             .filter(
-                group_memberships::country_id.eq(teams_by_country_name.get(&record.home_team).unwrap().country_id)
+                group_memberships::country_id.eq(teams_by_country_name
+                    .get(&record.home_team)
+                    .unwrap()
+                    .country_id),
             )
             .select((group_memberships::group_id, group_memberships::drawn_place))
             .first::<(i32, i16)>(conn)?;
 
-        let inserted_home_participant : MatchParticipant = insert_into(match_participants::table)
+        let inserted_home_participant: MatchParticipant = insert_into(match_participants::table)
             .values((
                 match_participants::stage_id.eq(1),
-                match_participants::country_id.eq(teams_by_country_name.get(&record.home_team).unwrap().country_id),
+                match_participants::country_id.eq(teams_by_country_name
+                    .get(&record.home_team)
+                    .unwrap()
+                    .country_id),
                 match_participants::group_id.eq(group_id),
                 match_participants::group_drawn_place.eq(drawn_place as i32),
             ))
@@ -251,15 +286,21 @@ fn import_matches(locations: Vec<Location>, countries: Vec<Country>, conn: &PgCo
 
         let drawn_place2 = group_memberships::table
             .filter(
-                group_memberships::country_id.eq(teams_by_country_name.get(&record.away_team).unwrap().country_id)
+                group_memberships::country_id.eq(teams_by_country_name
+                    .get(&record.away_team)
+                    .unwrap()
+                    .country_id),
             )
             .select(group_memberships::drawn_place)
             .first::<i16>(conn)?;
 
-        let inserted_away_participant : MatchParticipant = insert_into(match_participants::table)
+        let inserted_away_participant: MatchParticipant = insert_into(match_participants::table)
             .values((
                 match_participants::stage_id.eq(1),
-                match_participants::country_id.eq(teams_by_country_name.get(&record.away_team).unwrap().country_id),
+                match_participants::country_id.eq(teams_by_country_name
+                    .get(&record.away_team)
+                    .unwrap()
+                    .country_id),
                 match_participants::group_id.eq(group_id),
                 match_participants::group_drawn_place.eq(drawn_place2 as i32),
             ))
@@ -274,7 +315,10 @@ fn import_matches(locations: Vec<Location>, countries: Vec<Country>, conn: &PgCo
             .values((
                 matches::stage_id.eq(1),
                 matches::time.eq(time),
-                matches::location_id.eq(locations_by_city_and_stadium.get(&(record.city, record.stadium)).unwrap().location_id),
+                matches::location_id.eq(locations_by_city_and_stadium
+                    .get(&(record.city, record.stadium))
+                    .unwrap()
+                    .location_id),
                 matches::home_participant_id.eq(inserted_home_participant.match_participant_id),
                 matches::away_participant_id.eq(inserted_away_participant.match_participant_id),
             ))
@@ -297,29 +341,52 @@ fn main() {
 
     let db_connection = PgConnection::establish(&database_url).unwrap();
 
-    diesel::delete(schema::match_predictions::table).execute(&db_connection).expect("Clearing the match predictions table failed");
-    diesel::delete(schema::group_predictions::table).execute(&db_connection).expect("Clearing the group predictions table failed");
-    diesel::delete(schema::favourites::table).execute(&db_connection).expect("Clearing the favourites table failed");
+    diesel::delete(schema::match_predictions::table)
+        .execute(&db_connection)
+        .expect("Clearing the match predictions table failed");
+    diesel::delete(schema::group_predictions::table)
+        .execute(&db_connection)
+        .expect("Clearing the group predictions table failed");
+    diesel::delete(schema::favourites::table)
+        .execute(&db_connection)
+        .expect("Clearing the favourites table failed");
 
-    diesel::delete(schema::match_outcomes::table).execute(&db_connection).expect("Clearing the match outcomes table failed");
-    diesel::delete(schema::matches::table).execute(&db_connection).expect("Clearing the matches table failed");
+    diesel::delete(schema::match_outcomes::table)
+        .execute(&db_connection)
+        .expect("Clearing the match outcomes table failed");
+    diesel::delete(schema::matches::table)
+        .execute(&db_connection)
+        .expect("Clearing the matches table failed");
     // Note that in theory we can't clear all matches and match participants in one go, as they
     // depend on each other, in practice this script wont run after I've added match participants
     // that point to actual matches
     // Also I'm probably going to reset the database and add ON DELETE
-    diesel::delete(schema::match_participants::table).execute(&db_connection).expect("Clearing the match_participants table failed");
+    diesel::delete(schema::match_participants::table)
+        .execute(&db_connection)
+        .expect("Clearing the match_participants table failed");
 
-    diesel::delete(schema::group_memberships::table).execute(&db_connection).expect("Clearing the group memberships table failed");
-    diesel::delete(schema::groups::table).execute(&db_connection).expect("Clearing the groups table failed");
+    diesel::delete(schema::group_memberships::table)
+        .execute(&db_connection)
+        .expect("Clearing the group memberships table failed");
+    diesel::delete(schema::groups::table)
+        .execute(&db_connection)
+        .expect("Clearing the groups table failed");
 
-    diesel::delete(schema::locations::table).execute(&db_connection).expect("Clearing the locations table failed");
-    diesel::delete(schema::stages::table).execute(&db_connection).expect("Clearing the stages table failed");
-    diesel::delete(schema::countries::table).execute(&db_connection).expect("Clearing the countries table failed");
+    diesel::delete(schema::locations::table)
+        .execute(&db_connection)
+        .expect("Clearing the locations table failed");
+    diesel::delete(schema::stages::table)
+        .execute(&db_connection)
+        .expect("Clearing the stages table failed");
+    diesel::delete(schema::countries::table)
+        .execute(&db_connection)
+        .expect("Clearing the countries table failed");
 
     let locations = import_locations(&db_connection).expect("Import location table failed");
     import_stages(&db_connection).expect("Import stages table failed");
     let countries = import_countries(&db_connection).expect("Import Countries table failed");
     import_groups(&db_connection).expect("Import Groups table failed");
     import_group_memberships(&db_connection).expect("Import Group Memberships table failed");
-    import_matches(locations, countries, &db_connection).expect("Import Group Memberships table failed");
+    import_matches(locations, countries, &db_connection)
+        .expect("Import Group Memberships table failed");
 }
