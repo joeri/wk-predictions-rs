7b19562f8a5501bbb011baed1a3abfb4166ff88b
Joeri Samson <joerisamson@gmail.com>
2018-05-25 02:26:59 +0200

    Mix of formatting and expanding on things
    E.g. sort the list of users based on the score (now that we have that
    field)

--- src/web/dashboard.rs
+++ src/web/dashboard.rs
@@ -1,20 +1,29 @@
-use schema;
-use models::{User, Match};
+use models::{Match, MatchOutcome, MatchParticipant, MatchPrediction, User};
 use templates::{Context, TEMPLATE_SERVICE};
 use web::app_state::{AppState, DbExecutor};
 
 use actix::prelude::*;
 use actix_web::middleware::identity::RequestIdentity;
-use actix_web::{AsyncResponder, Error, Either, FutureResponse, HttpRequest, HttpResponse, State};
+use actix_web::{AsyncResponder, Either, Error, FutureResponse, HttpRequest, HttpResponse, State};
 use futures::Future;
 
-use diesel;
 use diesel::prelude::*;
 
 struct DashboardData {
     leader_board: Vec<User>,
-    finished: Vec<Match>,
-    upcoming: Vec<Match>,
+    finished: Vec<(
+        Match,
+        MatchParticipant,
+        MatchParticipant,
+        Option<MatchPrediction>,
+    )>,
+    upcoming: Vec<(
+        Match,
+        MatchParticipant,
+        MatchParticipant,
+        Option<MatchPrediction>,
+        Option<MatchOutcome>,
+    )>,
 }
 
 struct FetchDataForDashboard {
@@ -31,21 +40,20 @@ impl Handler<FetchDataForDashboard> for DbExecutor {
     fn handle(&mut self, _msg: FetchDataForDashboard, _: &mut Self::Context) -> Self::Result {
         use schema::users::dsl::*;
 
-
         // For now we return
         // Top 10 users (by score)
         // Most recent 6(?) matches, with outcomes (if already present) and predictions for current
         // player
         // 6 following upcoming matches, with predictions for current player
 
-
         // normal diesel operations
         let leaders = users
+            .order(score.asc())
             .limit(10)
             .get_results(&self.connection)
             .expect("Couldn't fetch leaderboard");
 
-        Ok(DashboardData { 
+        Ok(DashboardData {
             leader_board: leaders,
             finished: Vec::new(),
             upcoming: Vec::new(),
@@ -53,34 +61,47 @@ impl Handler<FetchDataForDashboard> for DbExecutor {
     }
 }
 
-pub fn index(request: HttpRequest<AppState>, state: State<AppState>) -> Either<FutureResponse<HttpResponse>, HttpResponse> {
+pub fn index(
+    request: HttpRequest<AppState>,
+    state: State<AppState>,
+) -> Either<FutureResponse<HttpResponse>, HttpResponse> {
     match request.identity() {
         Some(current_user_id) => {
-            Either::A(state
-                .db
-                .send(FetchDataForDashboard { user_id: current_user_id.parse().unwrap() })
-                .from_err()
-                .and_then(move |res| Ok(match res {
-                    Ok(dashboard_data) => {
-                        let mut context = Context::new();
-                        context.add("leader_board", &dashboard_data.leader_board);
-                        context.add("upcoming", &dashboard_data.upcoming);
-                        context.add("finished", &dashboard_data.finished);
+            println!("We have a user identity {:?}", current_user_id);
+            Either::A(
+                state
+                    .db
+                    .send(FetchDataForDashboard {
+                        user_id: current_user_id.parse().unwrap(),
+                    })
+                    .from_err()
+                    .and_then(move |res| {
+                        Ok(match res {
+                            Ok(dashboard_data) => {
+                                let mut context = Context::new();
+                                context.add("leader_board", &dashboard_data.leader_board);
+                                context.add("upcoming", &dashboard_data.upcoming);
+                                context.add("finished", &dashboard_data.finished);
 
-                        let rendered = TEMPLATE_SERVICE.render("dashboard.html", &context);
-                        match rendered {
-                            Ok(body) => HttpResponse::Ok().content_type("text/html").body(body),
-                            Err(error) => {
-                                println!("{}", error);
-                                HttpResponse::InternalServerError()
-                                    .content_type("text/html")
-                                    .body("Something went wrong")
+                                let rendered = TEMPLATE_SERVICE.render("dashboard.html", &context);
+                                match rendered {
+                                    Ok(body) => {
+                                        HttpResponse::Ok().content_type("text/html").body(body)
+                                    }
+                                    Err(error) => {
+                                        println!("{}", error);
+                                        HttpResponse::InternalServerError()
+                                            .content_type("text/html")
+                                            .body("Something went wrong")
+                                    }
+                                }
                             }
-                        }
-                    },
-                    Err(_) => HttpResponse::InternalServerError().into(),
-                })).responder())
-        },
+                            Err(_) => HttpResponse::InternalServerError().into(),
+                        })
+                    })
+                    .responder(),
+            )
+        }
         None => {
             let mut context = Context::new();
 
