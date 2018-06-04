9bdc3dfcb52a66d1587f307a5b2ca3266fcbf9b1
Joeri Samson <joerisamson@gmail.com>
2018-06-05 00:16:23 +0200

    Add code for the view

---
+++ migrations/2018-06-04-204848_add_view_for_match_info/up.sql
@@ -0,0 +1,34 @@
+-- Not sure that this really belongs in a migration, in that if we update the
+-- definition it becomes a mess to search for the latest definition.
+-- But it's the easiest method right now
+CREATE OR REPLACE VIEW full_match_infos
+AS SELECT
+  matches.match_id as match_id,
+  matches.location_id as location_id,
+  matches.time as time,
+
+  home_participant.group_id as home_group_id,
+  home_participant.group_drawn_place as home_group_drawn_place,
+  home_participant.previous_match_id as home_previous_match_id,
+  home_participant.result as home_previous_match_result,
+
+  home_country.name as home_country_name,
+  home_country.flag as home_country_flag,
+
+  away_participant.group_id as away_group_id,
+  away_participant.group_drawn_place as away_group_drawn_place,
+  away_participant.previous_match_id as away_previous_match_id,
+  away_participant.result as away_previous_match_result,
+
+  away_country.name as away_country_name,
+  away_country.flag as away_country_flag
+FROM
+  matches
+  INNER JOIN match_participants as home_participant
+    ON matches.home_participant_id = home_participant.match_participant_id
+  LEFT OUTER JOIN countries as home_country
+    ON home_participant.country_id = home_country.country_id
+  INNER JOIN match_participants as away_participant
+    ON matches.away_participant_id = away_participant.match_participant_id
+  LEFT OUTER JOIN countries as away_country
+    ON away_participant.country_id = away_country.country_id
