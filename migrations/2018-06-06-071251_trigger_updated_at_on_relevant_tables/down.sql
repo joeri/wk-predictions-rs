cd0361bc425c483b84f778fb1d7d04470f38b1a5
Joeri Samson <joerisamson@gmail.com>
2018-06-07 17:40:42 +0200

    Forgot rather important migration

---
+++ migrations/2018-06-06-071251_trigger_updated_at_on_relevant_tables/down.sql
@@ -0,0 +1,5 @@
+DROP TRIGGER set_updated_at ON favourites;
+DROP TRIGGER set_updated_at ON group_predictions;
+DROP TRIGGER set_updated_at ON match_outcomes;
+DROP TRIGGER set_updated_at ON match_predictions;
+DROP TRIGGER set_updated_at ON users;
---
+++ migrations/2018-06-06-071251_trigger_updated_at_on_relevant_tables/up.sql
@@ -0,0 +1,5 @@
+SELECT diesel_manage_updated_at('favourites');
+SELECT diesel_manage_updated_at('group_predictions');
+SELECT diesel_manage_updated_at('match_outcomes');
+SELECT diesel_manage_updated_at('match_predictions');
+SELECT diesel_manage_updated_at('users');
